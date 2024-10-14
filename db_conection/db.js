5958623b2735e5736584f81674dae6a0383ac757
Nicolás Tapié <nicoatapie@gmail.com>
2024-10-14 18:26:57 -0300

    add: 031 Agregando archivos de Eslint, pnpm
    Creando coneccion a base de datos de lowDb

---
+++ db_conection/db.js
@@ -0,0 +1,30 @@
+
+import { Low,JSONFile } from "lowdb";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import fs from 'fs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const createDb = (filePath) => {
+    const adapter = new JSONFile(filePath);
+    const db = new Low(adapter);
+    return db;
+};
+
+
+const loadAllDbs = () => {
+    const dbs = {};
+    const files = fs.readdirSync(join(__dirname, 'data'));
+    files.forEach(file => {
+        const dbName = file.replace('.json', '');
+        dbs[dbName] = createDb(join(__dirname, 'data', file));
+    });
+    return dbs;
+};
+
+
+
+export default loadAllDbs;
