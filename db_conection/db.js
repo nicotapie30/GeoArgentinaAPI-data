5ea5f3284e5df29a73134ee40099af2f2999fa71
Nicolás Tapié <nicoatapie@gmail.com>
2025-02-16 20:12:10 -0300

    add: 059 Agregando IDS Únicos a todas las provincias, departamentos y localidades

--- db_conection/db.js
+++ db_conection/db.js
@@ -1,31 +1,28 @@
-
 import { Low } from "lowdb";
-import { JSONFile } from 'lowdb/node';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import fs from 'fs';
+import { JSONFile } from "lowdb/node";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import fs from "fs";
 
+const URL = import.meta.url;
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(URL);
 const __dirname = dirname(__filename);
 
 const createDb = (filePath) => {
-    const adapter = new JSONFile(filePath);
-    const db = new Low(adapter,  { defaultValue: {} });
-    return db;
+  const adapter = new JSONFile(filePath);
+  const db = new Low(adapter, { defaultValue: {} });
+  return db;
 };
-
 
 const loadAllDbs = () => {
-    const dbs = {};
-    const files = fs.readdirSync(join(__dirname, '..', 'data'));
-    files.forEach(file => {
-        const dbName = file.replace('.json', '');
-        dbs[dbName] = createDb(join(__dirname, '..', 'data', file));
-    });
-    return dbs;
+  const dbs = {};
+  const files = fs.readdirSync(join(__dirname, "..", "data"));
+  files.forEach((file) => {
+    const dbName = file.replace(".json", "");
+    dbs[dbName] = createDb(join(__dirname, "..", "data", file));
+  });
+  return dbs;
 };
-
-
 
 export default loadAllDbs;
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-import express from 'express';
-import loadAllDbs from './db_conection/db.js';
+import express from "express";
+import loadAllDbs from "./db_conection/db.js";
 
 const app = express();
 app.use(express.json());
 
 const dbs = loadAllDbs();
 
-
 const PORT = 3000;
-const HOST = 'localhost'
-
+const HOST = "localhost";
 
 const getData = async (dbName) => {
   const db = dbs[dbName];
@@ -20,70 +18,84 @@ const getData = async (dbName) => {
   return null;
 };
 
-app.get('/api/:dbName', async (req, res) => {
+app.get("/api/:dbName", async (req, res) => {
   const data = await getData(req.params.dbName);
   if (data) {
     res.json(data);
   } else {
-    res.status(404).send('Database not found');
+    res.status(404).send("Database not found");
   }
 });
 
-app.get('/api/:dbName/departamentos', async (req, res) => {
+app.get("/api/:dbName/departamentos", async (req, res) => {
   const data = await getData(req.params.dbName);
   if (data) {
     res.json(data.departamentos);
   } else {
-    res.status(404).send('Database not found');
+    res.status(404).send("Database not found");
   }
 });
 
-app.get('/api/:dbName/departamentos/:departamento', async (req, res) => {
+app.get("/api/:dbName/departamentos/:departamento", async (req, res) => {
   const data = await getData(req.params.dbName);
   if (data) {
-    const departamento = data.departamentos.find(d => d.departamento === req.params.departamento);
+    const departamento = data.departamentos.find(
+      (d) => d.departamento === req.params.departamento
+    );
     if (departamento) {
       res.json(departamento);
     } else {
-      res.status(404).send('Departamento not found');
+      res.status(404).send("Departamento not found");
     }
   } else {
-    res.status(404).send('Database not found');
+    res.status(404).send("Database not found");
   }
 });
 
-app.get('/api/:dbName/departamentos/:departamento/localidades', async (req, res) => {
-  const data = await getData(req.params.dbName);
-  if (data) {
-    const departamento = data.departamentos.find(d => d.departamento === req.params.departamento);
-    if (departamento) {
-      res.json(departamento.localidades);
-    } else {
-      res.status(404).send('Departamento not found');
-    }
-  } else {
-    res.status(404).send('Database not found');
-  }
-});
-
-app.get('/api/:dbName/departamentos/:departamento/localidades/:localidad', async (req, res) => {
-  const data = await getData(req.params.dbName);
-  if (data) {
-    const departamento = data.departamentos.find(d => d.departamento === req.params.departamento);
-    if (departamento) {
-      const localidad = departamento.localidades.find(l => l.localidad === req.params.localidad);
-      if (localidad) {
-        res.json(localidad);
+app.get(
+  "/api/:dbName/departamentos/:departamento/localidades",
+  async (req, res) => {
+    const data = await getData(req.params.dbName);
+    if (data) {
+      const departamento = data.departamentos.find(
+        (d) => d.departamento === req.params.departamento
+      );
+      if (departamento) {
+        res.json(departamento.localidades);
       } else {
-        res.status(404).send('Localidad not found');
+        res.status(404).send("Departamento not found");
       }
     } else {
-      res.status(404).send('Departamento not found');
+      res.status(404).send("Database not found");
     }
-  } else {
-    res.status(404).send('Database not found');
   }
-});
+);
+
+app.get(
+  "/api/:dbName/departamentos/:departamento/localidades/:localidad",
+  async (req, res) => {
+    const data = await getData(req.params.dbName);
+    if (data) {
+      const departamento = data.departamentos.find(
+        (d) => d.departamento === req.params.departamento
+      );
+      if (departamento) {
+        const localidad = departamento.localidades.find(
+          (l) => l.localidad === req.params.localidad
+        );
+        if (localidad) {
+          res.json(localidad);
+        } else {
+          res.status(404).send("Localidad not found");
+        }
+      } else {
+        res.status(404).send("Departamento not found");
+      }
+    } else {
+      res.status(404).send("Database not found");
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${HOST}:${PORT}`);
